a655144be9724072469e805d0d55f4060de47c42
jamillepp <jamillepp@github.com>
2021-06-27 23:38:33 -0300

    last commit 2

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from mealy.mealy import mealyMachine
 
-# s = input()
-s = "Quem"
-p = " foi"
-o = " Stanley Kubrick"
+s = input("S: ")
+p = input("P: ")
+o = input("O: ")
 mealyMachine(s, p, o)
-
-# [Quem] [ministra] [a matéria talf?]
-#        [é capaz de passar em] [talf?]
-#        [é] [a Anya Taylor‑Joy?]
-#        [é a protagonista de] [O Gambito da Rainha?]
-#        [foi] [a Ada Lovelace?]
-#        [escreveu] [ o primeiro algoritmo de máquina?]
-#        [é o criador] [da máquina de Mealy?]
-#        [foi] [George H. Mealy?]
-#        [pode cursar] [Ciência da Computação?]
-#        [foi] [o Neil Armstrong?]
